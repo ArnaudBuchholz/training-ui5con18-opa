18a69fc0df8b4bd75a0aa9d603e4a3aa85ba2456
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-05-28 16:17:08 -0400

    Use iFrame mode to support parameters passing

--- webapp/test/integration/pages/Common.js
+++ webapp/test/integration/pages/Common.js
@@ -5,17 +5,16 @@ sap.ui.define([
 
 	return Opa5.extend("sap.ui.demo.todo.test.integration.pages.Common", {
 
-		iStartTheApp: function() {
-			this.iStartMyUIComponent({
-				componentConfig: {
-					name: "sap.ui.demo.todo",
-					async: true
-				}
+		iStartTheApp: function(oUrlParams) {
+			var aUrlParams = Object.keys(oUrlParams || {}).map(function (sParameterName) {
+				return sParameterName + "=" + encodeURIComponent(oUrlParams[sParameterName]);
 			});
+			aUrlParams.push(location.search.toString().substr(1)); // Also forward OPA parameters
+			return this.iStartMyAppInAFrame("../../index.html?" + aUrlParams.join("&"));
 		},
 
 		iTeardownTheApp: function() {
-			this.iTeardownMyUIComponent();
+			this.iTeardownMyAppFrame();
 		}
 
 	});
