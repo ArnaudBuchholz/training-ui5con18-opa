9b98056f6161e7097c0084c43cbcd6e5dd799021
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-05-31 17:12:31 -0400

    Isolate List APIs

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,6 +1,8 @@
 sap.ui.define([
 	"sap/ui/test/Opa5",
 	"sap/ui/demo/todo/test/integration/pages/Common",
+	"sap/ui/demo/todo/test/integration/pages/App",
+	"sap/ui/demo/todo/test/integration/pages/List",
 	"sap/ui/demo/todo/test/integration/TodoListJourney",
 	// "sap/ui/demo/todo/test/integration/SearchJourney",
 	// "sap/ui/demo/todo/test/integration/FilterJourney"
