e179f65e2bfc925aeb8930af8e6e242ca92d57f2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-06-02 14:06:55 -0400

    TODO

---
+++ webapp/test/integration/ErrorJourney.js
@@ -0,0 +1,12 @@
+sap.ui.require([
+	"sap/ui/test/opaQunit",
+	"sap/ui/demo/todo/test/integration/pages/Filters"
+], function (opaTest, filters) {
+	"use strict";
+
+    QUnit.skip("Error when creating a todo item");
+	QUnit.skip("Error when fetching a filter");
+	QUnit.skip("Error when completing an item");
+	QUnit.skip("Error when editing an item");
+
+});
