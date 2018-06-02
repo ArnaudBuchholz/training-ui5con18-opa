f80213881752b3f715373b4a45c078c8a2930b14
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-06-02 14:06:49 -0400

    Enables component mode

--- webapp/test/integration/pages/Common.js
+++ webapp/test/integration/pages/Common.js
@@ -1,20 +1,47 @@
 sap.ui.define([
-	"sap/ui/test/Opa5"
-], function(Opa5) {
+	"sap/ui/test/Opa5",
+	"sap/ui/demo/todo/test/MockServer"
+], function(Opa5, MockServer) {
 	"use strict";
+
+	var bInOpaPage = location.toString().indexOf("opaTests.qunit.html") !== -1 &&
+						jQuery.sap.getUriParameters().get("component") !== "true";
+
+	function _wrapParameters (oParameters) {
+		return {
+			get: function (name) {
+				return (oParameters[name] || "").toString();
+			}
+		};
+	}
 
 	return Opa5.extend("sap.ui.demo.todo.test.integration.pages.Common", {
 
-		iStartTheApp: function(oUrlParams) {
-			var aUrlParams = Object.keys(oUrlParams || {}).map(function (sParameterName) {
-				return sParameterName + "=" + encodeURIComponent(oUrlParams[sParameterName]);
-			});
-			aUrlParams.push(location.search.toString().substr(1)); // Also forward OPA parameters
-			return this.iStartMyAppInAFrame("../../index.html?" + aUrlParams.join("&"));
+		iStartTheApp: function(oParameters) {
+			if (bInOpaPage) {
+				var aUrlParams = Object.keys(oParameters || {}).map(function (sParameterName) {
+					return sParameterName + "=" + encodeURIComponent(oParameters[sParameterName]);
+				});
+				aUrlParams.push(location.search.toString().substr(1)); // Also forward OPA parameters
+				this.iStartMyAppInAFrame("../../index.html?" + aUrlParams.join("&"));
+
+			} else {
+				MockServer.init(_wrapParameters(oParameters || {}));
+				this.iStartMyUIComponent({
+					componentConfig: {
+						name: "sap.ui.demo.todo",
+						async: true
+					}
+				});
+			}
 		},
 
 		iTeardownTheApp: function() {
-			this.iTeardownMyAppFrame();
+			if (bInOpaPage) {
+				this.iTeardownMyAppFrame();
+			} else {
+				this.iTeardownMyUIComponent();
+			}
 		}
 
 	});
