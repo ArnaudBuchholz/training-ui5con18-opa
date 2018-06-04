f3eef6e40f630d2a259ae06db8c1e473b683924f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-06-04 13:11:35 -0400

    Error

--- webapp/test/integration/ErrorJourney.js
+++ webapp/test/integration/ErrorJourney.js
@@ -4,9 +4,64 @@ sap.ui.require([
 ], function (opaTest, filters) {
 	"use strict";
 
-    QUnit.skip("Error when creating a todo item");
-	QUnit.skip("Error when fetching a filter");
-	QUnit.skip("Error when completing an item");
+	var S_NEW_ITEM_TITLE = "my test",
+		S_STOP_PROCRASTINATING = "Stop procrastinating";
+
+	QUnit.module("Error");
+
+	opaTest("Error when creating a todo item", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp({
+			error: "new"
+		});
+
+		//Actions
+		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
+
+		// Assertions
+		Then.onTheAppPage.iShouldSeeAnError();
+
+		//Actions
+		When.onTheAppPage.iCloseTheError();
+
+		// Assertions
+		Then.onTheListOfItems.iShouldNotSeeAnyItemTitled(S_NEW_ITEM_TITLE)
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Error when fetching a filter", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp({
+			error: "filter"
+		});
+
+		//Actions
+
+		// Assertions
+		Then.onTheFilterButtons.iShouldSeeTheButtonCount(filters.ACTIVE, "-")
+			.and.iShouldSeeTheButtonCount(filters.LATE, "-")
+			.and.iShouldSeeTheButtonCount(filters.COMPLETED, "-")
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Error when completing an item", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp();
+
+		//Actions
+		When.onTheListOfItems.iSetTheItemToCompleted(S_STOP_PROCRASTINATING);
+
+		// Assertions
+		Then.onTheAppPage.iShouldSeeAnError();
+
+		//Actions
+		When.onTheAppPage.iCloseTheError();
+
+		// Assertions
+		Then.onTheListOfItems.iShouldSeeTheNewItem(S_STOP_PROCRASTINATING)
+			.and.iTeardownTheApp();
+	});
+
 	QUnit.skip("Error when editing an item");
 
 });
