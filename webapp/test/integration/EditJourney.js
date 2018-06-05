cfa17afa147f8a82d2d96361866ead40a9c2e2cb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-06-04 23:51:06 -0400

    Completes journey

--- webapp/test/integration/EditJourney.js
+++ webapp/test/integration/EditJourney.js
@@ -12,10 +12,78 @@ sap.ui.require([
 		// Arrangements
 		Given.iStartTheApp();
 
-		//Actions
+		// Actions
 		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
 		When.onTheListOfItems.iEditTheItem(S_NEW_ITEM_TITLE);
 
+		// Assertions
+		Then.onTheEditDialog.iShouldSeeTheDialog(S_NEW_ITEM_TITLE)
+			.and.iShouldSeeTheTitleField()
+			.and.iShouldSeeTheDueDateField()
+			.and.iShouldNotSeeTheCompletionDateField()
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Click save without changes", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp();
+
+		// Actions
+		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
+		When.onTheListOfItems.iEditTheItem(S_NEW_ITEM_TITLE);
+		When.onTheEditDialog.iClickSave();
+
+		// Assertions
+		Then.onTheListOfItems.iShouldSeeTheNewItem(S_NEW_ITEM_TITLE)
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Change the title of a new item", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp();
+
+		// Actions
+		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
+		When.onTheListOfItems.iEditTheItem(S_NEW_ITEM_TITLE);
+		When.onTheEditDialog.iSetTheTitleTo(S_NEW_ITEM_TITLE + " modified")
+			.and.iClickSave();
+
+		// Assertions
+		Then.onTheListOfItems.iShouldSeeTheNewItem(S_NEW_ITEM_TITLE + " modified")
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Change the title of a new item but close the dialog", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp();
+
+		// Actions
+		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
+		When.onTheListOfItems.iEditTheItem(S_NEW_ITEM_TITLE);
+		When.onTheEditDialog.iSetTheTitleTo(S_NEW_ITEM_TITLE + " modified")
+			.and.iClickClose();
+
+		// Assertions
+		Then.onTheListOfItems.iShouldSeeTheNewItem(S_NEW_ITEM_TITLE)
+			.and.iShouldNotSeeAnyItemTitled(S_NEW_ITEM_TITLE + " modified")
+			.and.iTeardownTheApp();
+	});
+
+	opaTest("Open a completed item", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartTheApp();
+
+		// Actions
+		When.onTheAppPage.iEnterTextForNewItemAndPressEnter(S_NEW_ITEM_TITLE);
+		When.onTheListOfItems.iSetTheItemToCompleted(S_NEW_ITEM_TITLE)
+			.and.iEditTheItem(S_NEW_ITEM_TITLE);
+
+		// Assertions
+		Then.onTheEditDialog.iShouldSeeTheDialog(S_NEW_ITEM_TITLE)
+			.and.iShouldNotSeeTheTitleField()
+			.and.iShouldSeeTheDueDateField()
+			.and.iShouldSeeTheCompletionDateField()
+			.and.iTeardownTheApp();
 	});
 
 });
