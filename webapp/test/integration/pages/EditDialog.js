a3f68090f5e3b73d6b86d7931099ec8115cf109c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-06-04 23:50:58 -0400

    Adds missing assertions & actions

--- webapp/test/integration/pages/EditDialog.js
+++ webapp/test/integration/pages/EditDialog.js
@@ -8,6 +8,35 @@ sap.ui.require([
 ], function (Opa5, Common, Properties, Press, EnterText) {
 	"use strict";
 
+	function _getIShouldSeeTheField (sId, sName) {
+		return function () {
+			return this.waitFor({
+				id: sId,
+				success: function () {
+					Opa5.assert.ok(true, "The " + sName + " field is displayed");
+				},
+				errorMessage: "The " + sName + " field is *not* displayed"
+			});
+		};
+	}
+
+	function _getIShouldNotSeeTheField (sId, sName) {
+		return function () {
+			return this.waitFor({
+				autoWait: false,
+				visible: false,
+				id: sId,
+				matchers: [new Properties({
+					visible: false
+				})],
+				success: function () {
+					Opa5.assert.ok(true, "The " + sName + " field is *not* displayed");
+				},
+				errorMessage: "The " + sName + " field is displayed"
+			});
+		};
+	}
+
 	Opa5.createPageObjects({
 		onTheEditDialog: {
 
@@ -15,9 +44,66 @@ sap.ui.require([
 
 			actions: {
 
+				iClickSave: function () {
+					return this.waitFor({
+						id: "btnOK",
+						actions: [new Press()],
+						success: function () {
+							Opa5.assert.ok(true, "Clicked save in the edit dialog");
+						},
+						errorMessage: "Unable to click save in the edit dialog"
+					});
+				},
+
+				iClickClose: function () {
+					return this.waitFor({
+						id: "btnCancel",
+						actions: [new Press()],
+						success: function () {
+							Opa5.assert.ok(true, "Clicked cancel in the edit dialog");
+						},
+						errorMessage: "Unable to click cancel in the edit dialog"
+					});
+				},
+
+				iSetTheTitleTo: function (sTitle) {
+					return this.waitFor({
+						id: "title",
+						actions: [new EnterText({
+							text: sTitle
+						})],
+						success: function () {
+							Opa5.assert.ok(true, "The title has been set to '" + sTitle + "'");
+						},
+						errorMessage: "Unable to set the title"
+					});
+				}
+
 			},
 
 			assertions: {
+
+				iShouldSeeTheDialog: function (sTitle) {
+					return this.waitFor({
+						searchOpenDialogs: true,
+						controlType: "sap.m.Dialog",
+						matchers: [new Properties({
+							title: sTitle
+						})],
+						success: function () {
+							Opa5.assert.ok(true, "The edit dialog is displayed");
+						},
+						errorMessage: "The edit dialog is not displayed"
+					});
+				},
+
+				iShouldSeeTheTitleField: _getIShouldSeeTheField("title", "title"),
+				iShouldNotSeeTheTitleField: _getIShouldNotSeeTheField("title", "title"),
+
+				iShouldSeeTheDueDateField: _getIShouldSeeTheField("dueDate", "due date"),
+
+				iShouldSeeTheCompletionDateField: _getIShouldSeeTheField("completionDate", "completion date"),
+				iShouldNotSeeTheCompletionDateField: _getIShouldNotSeeTheField("completionDate", "completion date")
 
 			}
 
