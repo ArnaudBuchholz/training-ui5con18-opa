55e8129a04c0c28d747c9cfa461ed1c1c7b8a58e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-08 14:15:13 -0400

    Basic setup script

--- workshop/setup.js
+++ workshop/setup.js
@@ -1,28 +1,57 @@
-"use strict";
+"use strict"
 
+require('colors')
 const fs = require('fs')
+const inquirer = require('inquirer')
 const { join } = require('path')
 const util = require('util')
 const [,,scenario] = process.argv
 
 const copyFile = util.promisify(fs.copyFile)
 const readdir = util.promisify(fs.readdir)
+const readFile = util.promisify(fs.readFile)
 const stat = util.promisify(fs.stat)
 
-console.log(`Setup of scenario '${scenario}'...`)
+console.log(`Setup of scenario '${scenario}'`.yellow)
 
-async function copy (path) {
-  const promises = [];
-  (await readdir(join(`./workshop/${scenario}/setup`, path))).forEach(async function (name) {
-    const subPath = join(path, name)
-    const srcPath = join(`./workshop/${scenario}/setup`, subPath)
-    if ((await stat(srcPath)).isDirectory()) {
-      promises.push(copy(subPath))
-    } else {
-      promises.push(copyFile(srcPath, join(`./`, subPath)))
-    }
-  })
-  return Promise.all(promises)
+function copy (folder = 'setup', path = '.') {
+  const srcBase = `./workshop/${scenario}/${folder}`
+  return readdir(join(srcBase, path))
+    .then(names => Promise.all(names
+      .map(name => {
+        const subPath = join(path, name)
+        const srcPath = join(srcBase, subPath)
+        return stat(srcPath)
+          .then(srcStat => {
+            if (srcStat.isDirectory()) {
+              return copy(folder, subPath)
+            } else {
+              console.log(`\t${subPath}`.gray)
+              return copyFile(srcPath, join(`./`, subPath))
+            }
+          })
+      })
+    ))
 }
 
-copy('.').then(() => console.log('done.'))
+function next (steps) {
+  return inquirer
+    .prompt([{
+      name: 'step',
+      type: 'list',
+      message: 'Which step?',
+      choices: steps.map(step => step.label)
+  }])
+    .then(answers => answers.step)
+    .then(stepLabel => {
+      const step = steps.filter(candidate => candidate.label === stepLabel)[0]
+      return copy(step.folder).then(() => next(steps))
+    })
+}
+
+readFile(`./workshop/${scenario}/steps.json`)
+  .then(stepsBuffer => JSON.parse(stepsBuffer.toString()))
+  .then(steps => copy()
+     .then(() => console.log('done.'.yellow))
+     .then(() => next(steps))
+  )
