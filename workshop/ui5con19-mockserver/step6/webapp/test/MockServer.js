ed8e78f336d121fa44f631b3a36efc88a69c1df4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-12 18:07:21 -0400

    Removes isolated code

--- workshop/ui5con19-mockserver/step6/webapp/test/MockServer.js
+++ workshop/ui5con19-mockserver/step6/webapp/test/MockServer.js
@@ -1,33 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/util/MockServer",
-	"sap/ui/demo/todo/const"
+	"sap/ui/demo/todo/const",
+	"./newItem"
 
-], function(MockServer, CONST) {
+], function(MockServer, CONST, getNewItem) {
 	"use strict";
 
 	var STOP_PROCRASTINATING_GUID = "0MOCKSVR-TODO-MKII-MOCK-00000000",
-		_lastTodoItemId = 0,
 		_oMockServer;
-
-	function _getNewItem (sTitle, dtDue, dtCompletion) {
-		var oItem = {},
-			sGuid = "0MOCKSVR-TODO-MKII-DYNK-" + (++_lastTodoItemId).toString().padStart(12,0);
-		oItem[CONST.OData.entityProperties.todoItem.guid] = sGuid;
-		oItem.__metadata = {
-			uri: "/odata/TODO_SRV/TodoItemSet(guid'" + sGuid + "')",
-			type: "TODO_SRV.TodoItem"
-		};
-		oItem[CONST.OData.entityProperties.todoItem.title] = sTitle;
-		oItem[CONST.OData.entityProperties.todoItem.dueDate] = "/Date(" + dtDue.getTime() + ")/";
-		if (dtCompletion) {
-			oItem[CONST.OData.entityProperties.todoItem.completionDate] = "/Date(" + dtCompletion.getTime() + ")/";
-			oItem[CONST.OData.entityProperties.todoItem.completed] = true;
-		} else {
-			oItem[CONST.OData.entityProperties.todoItem.completionDate] = null;
-			oItem[CONST.OData.entityProperties.todoItem.completed] = false;
-		}
-		return oItem;
-	}
 
 	return {
 
