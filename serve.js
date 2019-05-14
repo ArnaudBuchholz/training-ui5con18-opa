ae4541e8cf6355cd1cc74f547b7e397784e35c8e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-14 18:26:27 -0400

    Enables mock server and traces

--- serve.js
+++ serve.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const gpf = require('gpf-js')
 const inquirer = require('inquirer')
@@ -18,24 +18,39 @@ gpf.http.get(`${UI5BaseUrl}neo-app.json`)
     .then(answer => versions.filter(version => version.description === answer.version)[0])
   )
   .then(version => {
-    require('node-ui5/serve')({
-      port: 8080,
-      mappings: [{
-        // ui5 resource access
-        match: /\/resources\/(.*)/,
-        url: `${UI5BaseUrl}${version.path}/resources/$1`
-      }, {
-        // ui5 test-resource access
-        match: /\/test-resources\/(.*)/,
-        url: `${UI5BaseUrl}${version.path}/test-resources/$1`
-      }, {
-        // default access to index.html
-        match: /^\/$/,
-        file: path.join(__dirname, 'webapp/index.html')
-      }, {
-        // mapping to file access
-        match: /(.*)/,
-        file: path.join(__dirname, 'webapp', '$1')
-      }]
+    require('node-ui5/factory')({
+      verbose: true,
+      bootstrapLocation: `${UI5BaseUrl}${version.path}/resources/sap-ui-core.js`,
+      resourceroots: {
+        'sap.ui.demo.todo': path.join(__dirname, 'webapp')
+      }
+    }).then(({ sap, window }) => {
+      sap.ui.require([
+        'sap/ui/demo/todo/test/MockServer'
+      ], function (MockServer) {
+        MockServer.init({
+          get: name => name === 'randomize' ? '100' : undefined
+        })
+        require('node-ui5/serve')({
+          port: 8080,
+          mappings: [{
+            // ui5 resource access
+            match: /\/resources\/(.*)/,
+            url: `${UI5BaseUrl}${version.path}/resources/$1`
+          }, {
+            // ui5 test-resource access
+            match: /\/test-resources\/(.*)/,
+            url: `${UI5BaseUrl}${version.path}/test-resources/$1`
+          }, {
+            // default access to index.html
+            match: /^\/$/,
+            file: path.join(__dirname, 'webapp/index.html')
+          }, {
+            // mapping to file access
+            match: /(.*)/,
+            file: path.join(__dirname, 'webapp', '$1')
+          }]
+        })
+      })
     })
   })
