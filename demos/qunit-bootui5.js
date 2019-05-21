27f4687806f6cc97bda3e37d0a2068516e727d00
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-21 09:49:55 -0400

    Loading UI5 synchronously since qUnit does not support asynchronous code

--- demos/qunit-bootui5.js
+++ demos/qunit-bootui5.js
@@ -1,10 +1,18 @@
-console.log('Loading UI5...')
+'use strict'
+require('colors')
+const path = require('path')
+console.log('Loading UI5...'.gray)
 require('node-ui5/factory')({
   exposeAsGlobals: true,
-  verbose: true
+  fastButIncompleteSimulation: true,
+  // verbose: true,
+  resourceroots: {
+    'sap.ui.demo.todo': path.join(__dirname, '../webapp')
+  },
+  synchronousBoot: true // QUnit does not support async code to be tested
 })
   .then(() => {
-    console.log('UI5 ready.')
+    console.log('UI5 ready.'.gray)
   }, reason => {
     console.error(reason.toString())
   })
