1d1a794977a6b839f6d8d6bd6e370531c8d1caa6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-23 08:25:35 -0500

    Central way to list journeys

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,16 +1,13 @@
 sap.ui.require([
 	"sap/ui/test/Opa5",
 	"sap/ui/demo/todo/test/integration/pages/Common",
+	"sap/ui/thirdparty/qunit-2",
+	"sap/ui/qunit/qunit-junit",
+	"sap/ui/qunit/qunit-coverage",
 	"sap/ui/demo/todo/test/integration/pages/App",
 	"sap/ui/demo/todo/test/integration/pages/List",
 	"sap/ui/demo/todo/test/integration/pages/Filters",
-	"sap/ui/demo/todo/test/integration/pages/EditDialog",
-	"sap/ui/demo/todo/test/integration/TodoListJourney",
-	"sap/ui/demo/todo/test/integration/SearchJourney",
-	"sap/ui/demo/todo/test/integration/FilterJourney",
-	"sap/ui/demo/todo/test/integration/ErrorJourney",
-	"sap/ui/demo/todo/test/integration/EditJourney"
-
+	"sap/ui/demo/todo/test/integration/pages/EditDialog"
 ], function(Opa5, Common) {
 	"use strict";
 
@@ -21,4 +18,18 @@ sap.ui.require([
 		pollingInterval: 50 // Aggressive one to speed up demo
 	});
 
+	var sJourney = jQuery.sap.getUriParameters().get("journey");
+	var aJourneys = jQuery.sap.sjax({
+		type: "GET",
+		dataType: "json",
+		url: "./AllJourneys.json"
+	}).data
+	.filter(function(name) {
+		return !sJourney || sJourney === name;
+	})
+	.map(function(name) {
+		return "sap/ui/demo/todo/test/integration/" + name;
+	});
+
+	sap.ui.require(aJourneys);
 });
