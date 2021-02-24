25b6d6cb02bd2c8b6fae0881246e220b4241cb12
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-24 13:33:11 -0500

    Execute all tests in parallel and collect QUnit results

--- ci/test.js
+++ ci/test.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const { body, check, log, serve } = require('reserve')
+const { Request, Response, body, check, log, serve } = require('reserve')
 const { spawn } = require('child_process')
 const { randomInt } = require('crypto')
 
 const instance = {
   port: 8099,
   ui5: "https://ui5.sap.com/1.87.0",
-  browser: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-  browserOptions: "--no-sandbox --disable-gpu --remote-debugging-port=9222" //  --headless"
+  command: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+  options: "${url} --no-sandbox --disable-gpu --remote-debugging-port=9222 --headless",
+  parallel: 2
 }
 
 process.argv.forEach(arg => {
@@ -28,32 +29,90 @@ process.argv.forEach(arg => {
   }
 })
 
-function ack(response) {
-  response.writeHead(200)
-  response.end()
+function getPageResult (id) {
+  return instance._pageResults[instance._mapIdToPage[id]]
+}
+
+function endpoint (implementation) {
+  return async function (request, response) {
+    response.writeHead(200)
+    response.end()
+    const id = request.headers.referer.match(/__id__=(\d+)/)[1]
+    const data = JSON.parse(await body(request))
+    try {
+      await implementation.call(this, id, data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
 
 check({
   port: instance.port,
   mappings: [{
+    // Substitute qunit-redirect to extract test pages
     match: '/resources/sap/ui/qunit/qunit-redirect.js',
     file: './qunit-redirect.js'
   }, {
+    // Endpoint to receive test pages
     match: '/_/addTestPages',
-    custom: async (request, response) => {
-      instance.testPages = JSON.parse(await body(request))
-      const id = request.headers.referer.match(/__id__=(\d+)/)[1]
-      browse.close(id)
-      ack(response)
+    custom: endpoint((id, data) => {
+      instance._testPages = data
+      execute.kill(id)
+    })
+  }, {
+    // UI5 qunit.js source
+    match: '/_/qunit(-2)?.js',
+    url: `${instance.ui5}/resources/sap/ui/thirdparty/qunit$1.js`
+  }, {
+    // QUnit hooks
+    match: '/_/qunit-hooks.js',
+    file: './qunit-hooks.js'
+  }, {
+    // Concatenate qunit.js source with hooks
+    match: /\/thirdparty\/(qunit(?:-2)?\.js)/,
+    custom: async function (request, response, scriptName) {
+      const ui5Request = new Request('GET', `/_/${scriptName}`)
+      const ui5Response = new Response()
+      const hooksRequest = new Request('GET', '/_/qunit-hooks.js')
+      const hooksResponse = new Response()
+      await Promise.all([
+        this.configuration.dispatch(ui5Request, ui5Response),
+        this.configuration.dispatch(hooksRequest, hooksResponse)
+      ])
+      const hooksLength = parseInt(hooksResponse.headers['content-length'], 10)
+      const ui5Length = parseInt(ui5Response.headers['content-length'], 10)
+      response.writeHead(ui5Response.statusCode, {
+        ...ui5Response.headers,
+        'content-length': ui5Length + hooksLength,
+        'cache-control': 'no-store' // for debugging purpose
+      })
+      response.write(ui5Response.toString())
+      response.end(hooksResponse.toString())
     }
   }, {
-    match: "/(test-)resources/(.*)",
+    // Endpoint to receive QUnit test result
+    match: '/_/QUnit/testDone',
+    custom: endpoint((id, data) => {
+      getPageResult(id).tests.push(data)
+    })
+  }, {
+    // Endpoint to receive QUnit end
+    match: '/_/QUnit/done',
+    custom: endpoint((id, data) => {
+      getPageResult(id).report = data
+      execute.kill(id)
+    })
+  }, {
+    // UI5 resources
+    match: /\/(test-)?resources\/(.*)/,
     headers: {
-      Location: instance.ui5 + "/$1resources/$2"
+      location: `${instance.ui5}/$1resources/$2`
     },
     status: 302
   }, {
-    match: "^/(.*)",
+    // Project mapping
+    match: /^\/(.*)/,
     file: "../webapp/$1"
   }]
 })
@@ -64,34 +123,91 @@ check({
       })
   })
 
-function browse (relativeUrl) {
-  if (!browse._instances) {
-    browse._instances = {}
-    browse.close = id => {
-      const instance = browse._instances[id]
+function execute (relativeUrl) {
+  if (!execute._instances) {
+    execute._instances = {}
+    execute.kill = id => {
+      const instance = execute._instances[id]
       instance.process.kill("SIGKILL")
       instance.done()
-      delete browse._instances[id]
+      delete execute._instances[id]
     }
   }
 
+  if (!relativeUrl.startsWith('/')) {
+    relativeUrl = '/' + relativeUrl
+  }
+  if (relativeUrl.includes('?')) {
+    relativeUrl += '&'
+  } else {
+    relativeUrl += '?'
+  }
+
   const id = randomInt(0xFFFFFFFF)
-  const sep = relativeUrl.includes('?') ? '&' : '?'
-  const url = `http://localhost:${instance.port}/${relativeUrl}${sep}__id__=${id}`
+
+  const url = `http://localhost:${instance.port}${relativeUrl}__id__=${id}`
   console.log(`Opening ${url}`)
-  const process = spawn(instance.browser, [url].concat(instance.browserOptions.split(' ')), {
+  const process = spawn(instance.command, instance.options.split(' ').map(param => param === '${url}' ? url : param), {
     detached: true
   })
   let done
   const promise = new Promise(resolve => {
     done = resolve
   })
-  browse._instances[id] = { process, done }
+  execute._instances[id] = { process, done }
+  promise.id = id
   return promise
 }
 
 async function extractPages () {
-  await browse('test/testsuite.qunit.html')
-  console.log(instance.testPages)
-  process.exit(0)
+  await execute('test/testsuite.qunit.html')
+  console.log(instance._testPages)
+  instance._runningPages = []
+  instance._mapIdToPage = {}
+  instance._pageResults = {}
+  for (let i = 0; i < instance.parallel; ++i) {
+    runPage()
+  }
+}
+
+async function runPage () {
+  if (instance._runningPages.length === instance._testPages.length) {
+    if (!instance._waitingForEnd) {
+      instance._waitingForEnd = true
+      Promise.all(instance._runningPages).then(generateReport)
+    }
+    return
+  }
+  const index = instance._runningPages.length
+  const page = instance._testPages[index]
+  const promise = execute(page)
+  instance._runningPages.push(promise)
+  instance._mapIdToPage[promise.id] = page
+  instance._pageResults[page] = {
+    tests: []
+  }
+  promise.then(runPage)
+}
+
+async function generateReport () {
+  console.log('done.')
+
+  // Simple report
+  let failed = 0
+  instance._testPages.forEach(page => {
+    console.log(page)
+    const results = instance._pageResults[page]
+    console.table(results.tests.map(result => {
+      return {
+        name: result.name,
+        passed: result.passed,
+        failed: result.failed,
+        total: result.total
+      }
+    }))
+    console.log(results.report)
+    failed += results.report.failed
+  })
+
+  process.exit(failed)
 }
