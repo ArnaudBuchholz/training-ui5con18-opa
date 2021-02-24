32eb236b43217552ffa2befb35b789e9de4116b3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-24 17:28:22 -0500

    keep alive and post coverage info

--- ci/qunit-hooks.js
+++ ci/qunit-hooks.js
@@ -4,21 +4,27 @@
 
   var testCount = 0
 
+  function post (url, data) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('POST', '/_/' + url, false)
+    xhr.send(JSON.stringify(data))
+  }
+
   QUnit.testDone(function (report) {
     ++testCount
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', '/_/QUnit/testDone', false)
-    xhr.send(JSON.stringify(report))
+    post('QUnit/testDone', report)
   })
 
   QUnit.done(function (report) {
     if (!testCount) {
       return
     }
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', '/_/QUnit/done', false)
-    xhr.send(JSON.stringify(report))
-    // push coverage ?
-    window.close()
+    post('QUnit/done', report)
+    if (window.__coverage__) {
+      post('nyc/coverage', window.__coverage__)
+    }
+    if (!location.toString().includes('__keepAlive__')) {
+      window.close()
+    }
   })
 }())
