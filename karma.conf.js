3fce549b46521265f52fecf0ca1d2b719a113b37
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-09 07:55:16 -0500

    https://github.com/SAP/karma-ui5/blob/master/docs/migrate-v1.md

--- karma.conf.js
+++ karma.conf.js
@@ -3,13 +3,9 @@
 
 module.exports = function(config) {
 	config.set({
-
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: 'webapp',
-
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['qunit', 'openui5'],
+		frameworks: ['qunit', 'ui5'],
 
 		openui5: {
 			path: 'http://localhost:8080/resources/sap-ui-core.js'
