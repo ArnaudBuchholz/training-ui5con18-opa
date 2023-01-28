78ae2513f72263e7273b5c19d709a9fe222e7d3a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-28 13:24:47 -0500

    Compatibility with Karma Additional includes

--- webapp/load-ui5.js
+++ webapp/load-ui5.js
@@ -1,10 +1,23 @@
 (function () {
   "use strict";
-
-  var script = document.createElement("script");
+  var oninit = document.currentScript.dataset.oninit;
+  var includes = document.currentScript.dataset.includes;
+  var relPath = "";
+  var pathnames = location.pathname.split('/');
+  pathnames.pop(); // page url
+  while (pathnames.includes('test')) {
+    relPath += "../";
+    pathnames.pop();
+  }
+  if (!relPath) {
+    relPath = "./";
+  }
   var ui5base = "";
+  /* istanbul ignore next */ // Switch for github publishing
   if (location.toString().startsWith('https://')) {
     ui5base = "https://ui5.sap.com/1.109.0/";
+  } else {
+    ui5base = relPath;
   }
   var attributes = {
     id: "sap-ui-bootstrap",
@@ -16,12 +29,31 @@
     "data-sap-ui-compatVersion": "edge",
     "data-sap-ui-preload": "async",
     "data-sap-ui-resourceroots": JSON.stringify({
-      "sap.ui.demo.todo": "./"
+      "sap.ui.demo.todo": relPath
     }),
-    "data-sap-ui-oninit": "module:sap/ui/demo/todo/init"
+    "data-sap-ui-oninit": oninit
   };
+  document.write('<script ');
   Object.keys(attributes).forEach(function (name) {
-    script.setAttribute(name, attributes[name]);
+    document.write(name + "='" + attributes[name] + "'");
   });
-  document.head.appendChild(script);
+  document.write('></script>');
+  var handlers = {
+    ".js": function (include) {
+      document.write("<script src=\"" + relPath + "resources/" + include + "\"></script>");
+    },
+    ".css": function (include) {
+      document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + relPath + "resources/" + include + "\"></link>");
+    }
+  }
+  if (includes) {
+    includes
+      .split(",")
+      .map(function (include) {
+        return include.trim();
+      })
+      .forEach(function (include) {
+        handlers[include.match(/\.\w+$/)[0]](include)
+      });
+  }
 }());
