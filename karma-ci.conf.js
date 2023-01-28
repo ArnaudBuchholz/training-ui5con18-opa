0460303696565655521ad9cbb0808d218c5593df
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-28 00:31:36 -0500

    Reformat

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -1,29 +1,30 @@
+/* eslint-disable strict */
 module.exports = function(config) {
-	require("./karma.conf")(config);
-	config.set({
-		preprocessors: {
-			'webapp/{,!(test)}/*.js': ['coverage']
-		},
-		coverageReporter: {
-			includeAllSources: true,
-			reporters: [{
-				type: 'html',
-				dir: '../coverage/'
-			}, {
-				type: 'text'
-			}],
-			check: {
-				each: {
-					statements: 100,
-					branches: 100,
-					functions: 100,
-					lines: 100
-				}
-			}
-		},
-		reporters: ['progress', 'coverage'],
-		browsers: ['ChromeHeadless'],
-		singleRun: true
-	});
-	require("karma-ui5/helper").configureIframeCoverage(config);
+  require("./karma.conf")(config);
+  config.set({
+    preprocessors: {
+      'webapp/{,!(test)}/*.js': ['coverage']
+    },
+    coverageReporter: {
+      includeAllSources: true,
+      reporters: [{
+        type: 'html',
+        dir: '../coverage/'
+      }, {
+        type: 'text'
+      }],
+      check: {
+        each: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100
+        }
+      }
+    },
+    reporters: ['progress', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
+  require("karma-ui5/helper").configureIframeCoverage(config);
 };
