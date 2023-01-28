84b23d05dd1e314a8bae6b26d1c6a3d0cedf64bf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-28 13:25:19 -0500

    Use load-ui5, isolate scripts

--- webapp/test/unit/allTests.js
+++ webapp/test/unit/allTests.js
@@ -1,7 +1,28 @@
-var aTests = [
-	"sap/ui/demo/todo/test/unit/controller/App.controller"
-];
-if (typeof window.__karma__ === "undefined") {
-	aTests.push("sap/ui/demo/todo/test/unit/test/MockServer")
-}
-sap.ui.define(aTests);
+QUnit.config.autostart = false;
+QUnit.config.reorder = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"sap/ui/demo/todo/test/MockServer",
+	], function(MockServer) {
+		MockServer.init({
+			get: function(name) {
+				if (name === "randomize") {
+					return "100";
+				}
+				return "";
+			}
+		});
+		var aTests = [
+			"sap/ui/demo/todo/test/unit/controller/App.controller"
+		];
+		if (typeof window.__karma__ === "undefined") {
+			aTests.push("sap/ui/demo/todo/test/unit/test/MockServer")
+		}
+		sap.ui.require(aTests, function () {
+			QUnit.start();
+		});
+	});
+});
