d85c55900c4bdbbb4ff5adc7cbf443d2a0940a24
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-28 23:26:50 -0500

    UI5 update to 1.110.0

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
   config.set({
     frameworks: ['ui5'],
     ui5: {
-      url: "https://ui5.sap.com/1.109.0"
+      url: "https://ui5.sap.com/1.110.0"
     },    
     reporters: ['progress'],
     logLevel: config.LOG_INFO,
