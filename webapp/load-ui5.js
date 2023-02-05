dc635414e285a8b5fb06df0145ac1b8fe0701877
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-05 09:35:37 -0500

    Refactor load to improve compatibility

--- webapp/load-ui5.js
+++ webapp/load-ui5.js
@@ -1,8 +1,10 @@
 (function () {
   "use strict";
+
   var oninit = document.currentScript.dataset.oninit;
   var includes = document.currentScript.dataset.includes;
   var onlyIncludes = document.currentScript.dataset.onlyIncludes === "true";
+
   var relPath = "";
   var pathnames = location.pathname.split('/');
   pathnames.pop(); // page url
@@ -14,40 +16,45 @@
     relPath = "./";
   }
   var ui5base = "";
-  /* istanbul ignore next */ // Switch for github publishing
+  /* istanbul ignore next */ // Switch for online publishing
   if (location.toString().startsWith('https://')) {
     ui5base = "https://ui5.sap.com/1.110.0/";
   } else {
     ui5base = relPath;
   }
-  var attributes = {
-    id: "sap-ui-bootstrap",
-    type: "text/javascript",
-    src: ui5base + "resources/sap-ui-core.js",
-    "data-sap-ui-theme": "sap_belize",
-    "data-sap-ui-libs": "sap.m",
-    "data-sap-ui-bindingSyntax": "complex",
-    "data-sap-ui-compatVersion": "edge",
-    "data-sap-ui-preload": "async",
-    "data-sap-ui-resourceroots": JSON.stringify({
-      "sap.ui.demo.todo": relPath
-    }),
-    "data-sap-ui-oninit": oninit
-  };
-  if (!onlyIncludes) {
-    var ui5ScriptParts = Object.keys(attributes).map(function (name) {
+
+  function write (tagName, attributes) {
+    var parts = Object.keys(attributes).map(function (name) {
       return name + "='" + attributes[name] + "'";
     });
-    ui5ScriptParts.unshift("<script ")
-    ui5ScriptParts.push("><", "/script>\n")
-    document.write(ui5ScriptParts.join(""));
+    parts.unshift("<", tagName, " ");
+    parts.push("><", "/" + tagName + ">\n");
+    document.write(parts.join(""));
   }
+
+  if (!onlyIncludes) {
+    write("script", {
+      id: "sap-ui-bootstrap",
+      type: "text/javascript",
+      src: ui5base + "resources/sap-ui-core.js",
+      "data-sap-ui-theme": "sap_belize",
+      "data-sap-ui-libs": "sap.m",
+      "data-sap-ui-bindingSyntax": "complex",
+      "data-sap-ui-compatVersion": "edge",
+      "data-sap-ui-preload": "async",
+      "data-sap-ui-resourceroots": JSON.stringify({
+        "sap.ui.demo.todo": relPath
+      }),
+      "data-sap-ui-oninit": oninit
+    });
+  }
+
   var handlers = {
     ".js": function (include) {
-      document.write("<script src=\"" + ui5base + "resources/" + include + "\"><" + "/script>\n");
+      write("script", { src: ui5base + "resources/" + include });
     },
     ".css": function (include) {
-      document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + ui5base + "resources/" + include + "\"><" + "/link>\n");
+      write("link", { rel: "stylesheet", type: "text/css", href: ui5base + "resources/" + include });
     }
   }
   if (includes) {
