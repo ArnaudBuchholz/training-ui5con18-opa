806054c439a213a0ca1d13e47f5f7fc05ca14bbb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-05 09:36:26 -0500

    Refactor to improve compatibility

---
+++ webapp/test/unit/init.js
@@ -0,0 +1,25 @@
+sap.ui.require([
+	"sap/ui/demo/todo/test/MockServer",
+], function(MockServer) {
+	"use strict";
+
+	QUnit.config.reorder = false;
+
+	MockServer.init({
+		get: function(name) {
+			if (name === "randomize") {
+				return "100";
+			}
+			return "";
+		}
+	});
+	var aTests = [
+		"sap/ui/demo/todo/test/unit/controller/App.controller"
+	];
+	if (typeof window.__karma__ === "undefined") {
+		aTests.push("sap/ui/demo/todo/test/unit/test/MockServer")
+	}
+	sap.ui.require(aTests, function () {
+		QUnit.start();
+	});
+});
