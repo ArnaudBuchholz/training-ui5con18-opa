d1d3cafa354b3a60ea7ba4944fc904fa14e1dbf5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-03-28 06:50:21 -0400

    test: finalized version

--- webapp/test/integration/qunit-redirect.js
+++ webapp/test/integration/qunit-redirect.js
@@ -20,18 +20,41 @@
     }
   
     window.jsUnitTestSuite = jsUnitTestSuite
+
+    let QUnit;
+  
+    Object.defineProperty(window, 'QUnit', {
+      get: function () {
+        return QUnit
+      },
+  
+      set: function (value) {
+        QUnit = value
+
+        const { test } = QUnit
+        QUnit.test = (label) => test(label, (assert) => assert.ok(true, label))
+
+        let timeoutId
+        QUnit.moduleDone(function () {
+          if (timeoutId) {
+            clearTimeout(timeoutId)
+          }
+          timeoutId = setTimeout(notify, 10)
+        })
+
+        function notify () {
+          const modules = QUnit.config.modules.map(({ moduleId }) => moduleId)
+          const opa = !!window?.sap?.ui?.test?.Opa5
+          console.log('addTestPages\n' + JSON.stringify({ type: 'qunit', opa, modules, page: location.toString() }))
+        }
+      }
+    })
   
     window.addEventListener('load', function () {
       if (typeof suite === 'function') {
         suite()
         alert('addTestPages\n' + JSON.stringify({ type: 'suite', pages }))
-      } else if (typeof QUnit === 'object') {
-        QUnit.testStart(function (details) {
-          const modules = QUnit.config.modules.map(({ moduleId }) => moduleId)
-          const opa = !!window?.sap?.ui?.test?.Opa5
-          alert('addTestPages\n' + JSON.stringify({ type: 'qunit', opa, modules, page: location.toString() }))
-        })
-      } else {
+      } else if (!QUnit) {
         alert('addTestPages\n' + JSON.stringify({ type: 'none ' }))
       }
     })
